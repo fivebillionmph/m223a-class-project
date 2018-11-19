f20cb1990af9d8366aaf51aae6bf57e69bb52bf7
aogarlid <aogarlid@gmail.com>
2018-11-19 11:56:09 -0800

    update wrapper code
    update wrapper code with annotations, additional inputs (signals).
    
    Signed-off-by: aogarlid <aogarlid@gmail.com>

--- anders/brain_db-inputs.py
+++ anders/brain_db-inputs.py
@@ -1,19 +1,21 @@
-import psycopg2
-#from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+# requests initial file inputs from the user, as well as subject name and type
 
-name=input("Please enter subject name.")
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+name=input("Please enter subject name. ")
 
 conn=psycopg2.connect(dbname='brain_db',user='postgres',password='pass')
-#conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cursor=conn.cursor()
 
-sql = """SELECT sid from subjects WHERE name=%s"""
-cursor.execute(sql, (name,))
+select_subject = """SELECT sid from subjects WHERE name=%s"""
+cursor.execute(select_subject, (name,))
 
 subject_names = cursor.fetchall()
 
 if len(subject_names) == 0:
-    expt_type=input("Please enter experiment type (EEG/ECoG).")
+    expt_type=input("Please enter experiment type (EEG/ECoG). ")
     # add a 'while' loop to demand only EEG or ECOG as input.
     
     # hard coded path to standard brain MR
@@ -21,18 +23,18 @@ if len(subject_names) == 0:
     ct_path = ""
     
     if expt_type == "ECoG":
-        mr=input("Do you have an MR file for this subject? (y/n)")
+        mr=input("Do you have an MR file for this subject? (y/n) ")
         if mr == "y":
-            mr_path = input("What is the MR file path?")
+            mr_path = input("What is the MR file path? ")
 
-        ct=input("Do you have a CT file for this subject? (y/n)")   
+        ct=input("Do you have a CT file for this subject? (y/n) ")   
         if ct == "y":
-            ct_path = input("What is the CT file path?")
+            ct_path = input("What is the CT file path? ")
         else:
-            ct_path = input("What is the excel file path with electrode coords?")
+            ct_path = input("What is the excel file path with electrode coords? ")
         
-    sql = """INSERT INTO subjects(name,type,mr_path,ct_path) VALUES(%s,%s,%s,%s) RETURNING sid;"""
-    cursor.execute(sql, (name,expt_type,mr_path,ct_path))        
+    insert_subject = """INSERT INTO subjects(name,type,mr_path,ct_path) VALUES(%s,%s,%s,%s) RETURNING sid;"""
+    cursor.execute(insert_subject, (name,expt_type,mr_path,ct_path))        
     # get subject ID
     sid = cursor.fetchone()[0]
 
@@ -41,6 +43,14 @@ else:
     
 # commit the transaction
 conn.commit()
+
+# developing signal insertion: 
+
+# insert_signals = """INSERT INTO signals(sid,signal_path) VALUES(%s,%s) RETURNING sid;"""
+# cursor.execute(insert_signals, (sid,signal_path))
+
+
+
 # close the database communication
 cursor.close()
 
