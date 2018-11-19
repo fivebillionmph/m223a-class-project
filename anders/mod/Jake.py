dbd1e8bd47b897e8beffe964195e6746ff786717
jakepensa <44854626+jakepensa@users.noreply.github.com>
2018-11-19 13:52:56 -0800

    Add files via upload

---
+++ anders/mod/Jake.py
@@ -0,0 +1,6 @@
+#Power Analysis
+
+import os
+
+def run (filename, startTime, stopTime, startFrequency, stopFrequency):
+    os.system("..//..//Jake//Power_Analysis.exe" + " " + filename + " " + startTime + " " + stopTime + " " + startFrequency + " " + stopFrequency)
