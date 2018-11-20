b13f6c8a12719afd5089a1e923a679b36a4b04a6
Mohammadali Alidoost <44854909+malidoost@users.noreply.github.com>
2018-11-19 17:47:24 -0800

    Update pac.py

--- anders/mod/mohammad/pac.py
+++ anders/mod/mohammad/pac.py
@@ -5,5 +5,5 @@ import matlab.engine
 
 def run ():
     eng = matlab.engine.start_matlab()
-    pac = eng.pac (r'file', matlab.double([2, 14]), matlab.double([40, 150]), nargout=2)
+    pac = eng.pac (r'file', matlab.double([2, 14]), matlab.double([40, 150]), nargout=1)
     print(pac)
