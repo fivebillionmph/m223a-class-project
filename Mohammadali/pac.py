dda8f2a913ea174c386c94761113f341e40f1ecc
Mohammadali Alidoost <44854909+malidoost@users.noreply.github.com>
2018-11-20 00:28:22 -0800

    Add files via upload

--- Mohammadali/pac.py
+++ Mohammadali/pac.py
@@ -1,4 +1,5 @@
 import matlab.engine
 eng = matlab.engine.start_matlab()
-pac = eng.pac (r'file', matlab.double([2, 14]), matlab.double([40, 150]), nargout=1)
+pac = eng.pac (r'file', matlab.double([2, 14]), matlab.double([40, 150]), nargout=2)
 print(pac)
+
