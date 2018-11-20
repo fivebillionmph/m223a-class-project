3e53f67d51f93863a355e5cc4092590d8a1a0132
Mohammadali Alidoost <44854909+malidoost@users.noreply.github.com>
2018-11-20 00:37:56 -0800

    Update pac.m

--- anders/mod/mohammad/pac.m
+++ anders/mod/mohammad/pac.m
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%% Phase Amplitude Coupling (PAC) %%%%%
-function [MaxPAC, MeanPAC] = pac(filename, LowFreqs, HighFreqs)
+function [MaxPAC, MeanPAC] = pac(file, LowFreqs, HighFreqs)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 isUseParallel = 0;
@@ -9,22 +9,22 @@ numfreqs = 0;
 
 %%%%% Input Data %%%%%
 
-[~, ~, ext] = fileparts(filename);
+[filepath, name, ext] = fileparts(file);
 
 if strcmpi(ext, '.dat')
     % % % EEG&ECOG
-    [signal, ~, parameters, ~] = load_bcidat (filename);
+    [signal, ~, parameters, ~] = load_bcidat (file);
     F = transpose (signal);
     sRate = parameters.SamplingRate.NumericValue;
     % % % Using just 1 min. of ECOG! (otherwise, Out of memory (OOM))
-%     F = F(1,1:576000); % % % time: [(1/9600)*X(iteration)=60s]
+%     F = F(:,1:576000); % % % time: [(1/9600)*X(iteration)=60s]
 elseif strcmpi(ext, '.edf')
      % % % .edf file format
-     [hdr, record]= edfread(filename);
+     [hdr, record]= edfread(file);
      F = record;
      sRate = hdr.frequency(1,1);
      % % % Using just 2 min. of EDF!
-     F = F(1:1,1:240000); % % % time: [(1/2000)*X(iteration)=120s]
+     F = F(:,1:240000); % % % time: [(1/2000)*X(iteration)=120s]
 else
     Error ('Invalid File Format');
 end
@@ -74,10 +74,12 @@ end
 
 MaxPAC = transpose (MaxPAC);
 MeanPAC = transpose (MeanPAC);
+Output = [MaxPAC, MeanPAC];
 
-csvwrite('Max_PAC.dat',MaxPAC);
-csvwrite('Mean_PAC.dat',MeanPAC);
+% csvwrite('Max_PAC.dat',MaxPAC);
+% csvwrite('Mean_PAC.dat',MeanPAC);
+
+csvfile = strcat(filepath,'\',name,'.csv');
+csvwrite(csvfile, Output);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
