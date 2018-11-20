408a9e64ae2833f05ce33859ddf98a94729f8661
Mohammadali Alidoost <44854909+malidoost@users.noreply.github.com>
2018-11-19 17:41:18 -0800

    Update pac.py

--- anders/mod/mohammad/pac.py
+++ anders/mod/mohammad/pac.py
@@ -5,5 +5,5 @@ import matlab.engine
 
 def run ():
     eng = matlab.engine.start_matlab()
-    pac = eng.pac (r'C:\Users\alido\Desktop\2\EEG1.dat', matlab.double([2, 14]), matlab.double([40, 150]), nargout=2)
+    pac = eng.pac (r'file', matlab.double([2, 14]), matlab.double([40, 150]), nargout=2)
     print(pac)
