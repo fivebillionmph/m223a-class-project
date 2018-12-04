81e117925b236c19e2ec12d775d44938a4174e59
aogarlid <aogarlid@gmail.com>
2018-12-04 13:36:54 -0800

    cleanup wrapper.py, resolve merge conflicts
    Signed-off-by: aogarlid <aogarlid@gmail.com>

--- anders/wrapper.py
+++ anders/wrapper.py
@@ -12,12 +12,12 @@ except:
     pass
 
 # request subject name from user.
-name=input("Please enter subject name. ")
+name = input("Please enter subject name. ")
 
 # connect to brain_db and establish cursor connection.
-conn=psycopg2.connect(dbname='brain_db',user='postgres',password='pass')
+conn = psycopg2.connect(dbname='brain_db',user='postgres',password='pass')
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-cursor=conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
+cursor = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
 
 # query brain_db for existing subject name and select subject.
 select_subject = """SELECT sid, type from subjects WHERE name=%s"""
@@ -28,7 +28,7 @@ subject_names = cursor.fetchall()
 #### ACQUIRE SUBJECT NAME, EXPT TYPE, FILE INPUTS
 if len(subject_names) == 0:
     # if subject name does not exist, request name, experiment type, and image paths.
-    expt_type=input("Please enter experiment type (EEG/ECoG). ")
+    expt_type = input("Please enter experiment type (EEG/ECoG). ")
     # add a 'while' loop to demand only EEG or ECOG as input.
     
     # hard coded path to standard brain MR, standard brain SMR, standard EEG, blank CT.
@@ -39,11 +39,11 @@ if len(subject_names) == 0:
 
     # acquire file paths based on experiment type and available images.
     if expt_type == "ECoG":
-        mr=input("Do you have an MR file for this subject? (y/n) ")
+        mr = input("Do you have an MR file for this subject? (y/n) ")
         if mr == "y":
             mr_path = input("Please enter the MR file path: ")
 
-        ct=input("Do you have a CT file for this subject? (y/n) ")   
+        ct = input("Do you have a CT file for this subject? (y/n) ")
         if ct == "y":
             ct_path = input("Please enter the CT file path: ")
         else:
@@ -59,7 +59,7 @@ else:
     sid = subject_names[0]["sid"]
     expt_type = subject_names[0]["type"]
     
-# commit the transaction to add content to subject relation.
+# commit the transaction to add content to subjects relation.
 conn.commit()
 
 
@@ -83,6 +83,8 @@ insert_signals = """INSERT INTO signals(sid,signal_path) VALUES(%s,%s);"""
 for path in signals:
     cursor.execute(insert_signals, (sid,path))
 
+# commit the transaction to add content to signals relation.
+conn.commit()
 
 #### FILL CHANNEL TABLE WITH COORDINATES FROM EEG TABLE
 if expt_type == "EEG":
@@ -98,7 +100,7 @@ if expt_type == "EEG":
             eeg_row = cursor.fetchall()[0]            
             cursor.execute(insert_eeg_channel, (sid, row[0],eeg_row[0]))
 
-# commit the transaction
+# commit the transaction to add content to channels table.
 conn.commit()
 
 #select_ecog_channel = """SELECT * FROM channels WHERE sid=%s;"""
@@ -106,25 +108,6 @@ conn.commit()
 #for channel in channels:
 
 
-<<<<<<< HEAD
-=======
-
-#### SIGNAL ANALYSIS
-# choose signal analysis method
-# set up so you can choose multiple or only one to run
-method = input("Enter the signal processing method that you would like to use (1, 2, 3, or 4).")
-
-if method == '4':
-    band_lo = input("Please enter the desired low bandwidth range between 1 and 40 Hz (e.g. \"2, 14\"). ")
-    band_hi = input("Please enter the desired low bandwidth range between 40 and 200 Hz (e.g. \"40, 200\"). ")
-    ch_count = input("Please enter the number of signal channels you would like to process (enter 0 if you want all channels processed). ")
-    ch_first = input("Please enter the number of the first signal channel to be processed. ")
-    ch_last = input("Please enter the number of the last signal channel to be processed. ")
-    sigtime_total = input("Please enter the total range of time (in minutes) you would like to process (enter 0 if you want to process entire range of time). ")
-    sigtime_window = input("Please enter the short time window you would like to process (in minutes). ")
-    sigtime_step = input("Please enter the short time step you would like to process (in minutes). ")
-
->>>>>>> 1ff58680baf0ff7bbba34770473c4913ae4a4d20
 #### RUN INDIVIDUAL COMPONENT SCRIPTS
 # SKULL STRIPPING
 # Yannan.run(cursor, sid, config.brainsuite_cortical_extraction_script, mr_path)
@@ -145,35 +128,17 @@ if method == 1:
     David.run(cursor, sid, eeg_file)
 if method == 2:
     Amy.run(cursor, sid, eeg_file)
-if method == 3:
-    Jake.run(cursor, sid, eeg_file)
-# Jake has two methods: one for EDF and one for DAT
-    # can specify time ranges and frequency bands of expt 
+
+# Jake signal processing: two methods (one for EDF and one for DAT)
+    # can specify time ranges and frequency bands of expt
     # results in different scores (to be added as JakeMethod1a, JakeMethod1b)
     # method name might take format: "Jake_[time sequence]_[frequency band]"
     # will have to prompt user to specify their time ranges and frequency band
-<<<<<<< HEAD
-if method == '4':
-    band_lo = input("Please enter the desired low bandwidth range between 1 and 40 Hz (e.g. \"2, 14\"). ")
-    band_hi = input("Please enter the desired low bandwidth range between 40 and 200 Hz (e.g. "40, 200"). ")
-    ch_count = input("Please enter the number of signal channels you would like to process (enter 0 if you want all channels processed). ")
-    ch_first = input("Please enter the number of the first signal channel to be processed. ")
-    ch_last = input("Please enter the number of the last signal channel to be processed. ")
-    sigtime_total = input("Please enter the total range of time (in minutes) you would like to process (enter 0 if you want to process entire range of time). ")
-    sigtime_window = input("Please enter the short time window you would like to process (in minutes). ")
-    sigtime_step = input("Please enter the short time step you would like to process (in minutes). ")
 
-    mod.mohammad.pac.run(cursor, sid, eeg_file, band_lo, band_hi, ch_count,
-                         ch_first, ch_last, sigtime_total, sigtime_window, sigtime_step)
-=======
-try:
-    if method == 4:
-        mod.mohammad.pac.run(cursor, sid, eeg_file, band_lo, band_hi, ch_count,
-                             ch_first, ch_last, sigtime_total, sigtime_window, sigtime_step)
-except:
-    pass
->>>>>>> 1ff58680baf0ff7bbba34770473c4913ae4a4d20
-# mohammad inputs
+if method == 3:
+    Jake.run(cursor, sid, eeg_file)
+
+# Mohammad signal processing
 # bandwidth (ranges)
     # low (1:40 Hz)
     # high (40:200 Hz)
@@ -188,6 +153,21 @@ except:
         # "enter 0 if you want full range of time"
     # time window
     # time step
+
+try:
+    if method == '4':
+        band_lo = input("Please enter the desired low bandwidth range between 1 and 40 Hz (e.g. \"2, 14\"). ")
+        band_hi = input("Please enter the desired low bandwidth range between 40 and 200 Hz (e.g. \"40, 200\"). ")
+        ch_count = input("Please enter the number of signal channels you would like to process (enter 0 if you want all channels processed). ")
+        ch_first = input("Please enter the number of the first signal channel to be processed. ")
+        ch_last = input("Please enter the number of the last signal channel to be processed. ")
+        sigtime_total = input("Please enter the total range of time (in minutes) you would like to process (enter 0 if you want to process entire range of time). ")
+        sigtime_window = input("Please enter the short time window you would like to process (in minutes). ")
+        sigtime_step = input("Please enter the short time step you would like to process (in minutes). ")
+        mod.mohammad.pac.run(cursor, sid, eeg_file, band_lo, band_hi, ch_count,
+                             ch_first, ch_last, sigtime_total, sigtime_window, sigtime_step)
+except:
+    pass
 
 #### HEATMAP GENERATION
 # Aaron.run
