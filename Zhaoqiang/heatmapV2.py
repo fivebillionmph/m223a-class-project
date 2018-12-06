211740b90d2c56eb2578c4cc0e6b470325179b4d
Aaron Wang <32904974+aaronzq@users.noreply.github.com>
2018-12-06 14:11:52 -0800

    Add files via upload

--- Zhaoqiang/heatmapV2.py
+++ Zhaoqiang/heatmapV2.py
@@ -12,6 +12,7 @@ from mayavi.core.ui.api import MayaviScene, SceneEditor, MlabSceneModel
 
 import pandas as pd
 
+import ecogcorr as ecCorr
 
 
 NO_READIN = 0
@@ -49,101 +50,77 @@ def diluteMask(mask):
     volDilute = calcVolume(dMask)
     return dMask,volOri,volDilute
 
-def readScore(filename):
-    # For CSV format
-    # This readScore function now only read one row of values
-    # thus ONLY for one time point, revise it after we have multi-timestep data
+def readScore(scoreTable):
+    # For database table
+    # return score(2d numpy array, m channel X n time), channel num, timestep num
 
-    table = pd.read_csv(filename)
-    scoreStr = table.columns.tolist()
-    score = []
-    for s in scoreStr:
-        try:
-            f = float(s)
-        except ValueError:
-            # this is to handle some abnormal values in csv file like '123.41.21'
-            point = [i for i,v in enumerate(s) if v=='.']
-            f = s[0:point[1]]
-            f = float(f)
-        
-        score.append(f)
+    #######
+    # transform score into 2d numpy here
+    # score
+    #######
     
-    scoreNorm = [sc/max(score)*50000 for sc in score] #Normalize the score
+    #scoreNorm = [s/(score.max)*50000 for s in score] #Normalize the score
 
-    return scoreNorm,len(scoreNorm)
+    #return scoreNorm,scoreNorm.shape
+    return '','',''
 
-def readPos(filename):
-    # For CSV format
-    # This readPos function read 3 rows of positions: X, Y and Z
+def readPos(posTable):
+    # For database table 
+    # return 1d list x,y and z
 
-    table = pd.read_csv(filename)
-    xtable = table.loc[0].tolist()
-    ytable = table.loc[1].tolist()
-    ztable = table.loc[2].tolist()
-    x = [int(i) for i in xtable]
-    y = [int(i) for i in ytable]
-    z = [int(i) for i in ztable]
-    return x,y,z
+    # return x,y,z
+    return '','',''
 
-def locateElectron(space,score,x,y,z):
-    N = len(x)
-    
+def locateElectron(space3d,score1d,X,Y,Z):
+    # space3d: 3d numpy array
+    # score1d: 1d numpy array
+    # X,Y,Z: 1d list
+    N = len(X)  
     for i in range(0,N):
-        space[x[i],y[i],z[i]] = score[i]
+        space3d[X[i],Y[i],Z[i]] = score1d[i]
 
-    return space
+    return space3d
 
-def convolutionHeatmap(bSpace,electronScore,electronX,electronY,electronZ,mask):
-    eSpace = locateElectron(bSpace,electronScore,electronX,electronY,electronZ)
-    cSpace = ndimage.gaussian_filter(eSpace,5)
-    return cSpace*mask
+def convolutionHeatmap(timestep,row,col,depth,score2d,X,Y,Z,mask):
+    # score2d: 2d numpy array, m channel X n time
+   
+    heatmap = np.zeros([timestep,row,col,depth])
+    for t in range(timestep):
+        blankSpace = np.zeros([row,col,depth])
+        electrodeSpace = locateElectron(blankSpace, score2d[:,t], X, Y, Z)
+        convSpace = ndimage.gaussian_filter(electrodeSpace,5)
+        heatmap[t] = convSpace * mask
+
+    return heatmap
 
 class HeatMap:
-    def __init__(self, mriBrainMask):
+    def __init__(self, mriBrainMask, posTable, scoreTable):
         brain = nib.load(mriBrainMask)
         self.brainMask = createMask(brain.get_fdata())
         self.brainDMask, volOri, volDilute = diluteMask(self.brainMask)
         self.row, self.col, self.depth = self.brainMask.shape
         print('MRI brain mask loaded successfully')
 
-        self.mapSpace = np.zeros([self.row, self.col, self.depth])
-        self.EEG_POS_FLAG = NO_READIN
-        self.EEG_SCORE_FLAG = NO_READIN
-        self.ECOG_POS_FLAG = NO_READIN
-        self.ECOG_SCORE_FLAG = NO_READIN
+        self.POS_FLAG = NO_READIN
+        self.SCORE_FLAG = NO_READIN
 
+        # PosX is a m channel list
+        self.PosX, self.PosY, self.PosZ = readPos(posTable)
+        self.POS_FLAG = YES_READIN
+        print('Load Electrodes positions successfully')
 
-    def readEEG_Pos(self,filename):
-        self.EEG_PosX, self.EEG_PosY, self.EEG_PosZ = readPos(filename)
-        self.EEG_POS_FLAG = YES_READIN
-        print('Load EEG electrodes positions successfully')
-
-    def readECOG_Pos(self,filename):
-        self.ECOG_PosX, self.ECOG_PosY, self.ECOG_PosZ = readPos(filename)
-        self.ECOG_POS_FLAG = YES_READIN
-        print('Load ECoG electrodes positions successfully')
-
-    def readEEG_Score(self,filename):
-        self.EEG_Score, self.EEG_ChannelNum = readScore(filename)
-        self.EEG_SCORE_FLAG = YES_READIN
-        print('Load EEG scores successfully')
-
-    def readECOG_Score(self,filename):
-        self.ECOG_Score, self.ECOG_ChannelNum = readScore(filename)
-        self.ECOG_SCORE_FLAG = YES_READIN
-        print('Load ECoG scores successfully')
+        # score is m channel x n time numpy array
+        self.Score, self.ChannelNum, self.timestep = readScore(scoreTable)
+        self.SCORE_FLAG = YES_READIN
+        print('Load Scores successfully')
 
     def generateHeatmap(self):
-        if not (self.EEG_POS_FLAG and self.EEG_SCORE_FLAG and self.ECOG_POS_FLAG and self.ECOG_SCORE_FLAG):
-            print('Requiring data missing. Flag:  ', self.EEG_POS_FLAG, self.EEG_SCORE_FLAG, self.ECOG_POS_FLAG, self.ECOG_SCORE_FLAG)
+        if not (self.POS_FLAG and self.SCORE_FLAG):
+            print('Requiring data missing. Flag:  ', self.POS_FLAG, self.SCORE_FLAG)
         else:
-            self.eegHeatmap = convolutionHeatmap(self.mapSpace,self.EEG_Score,self.EEG_PosX,self.EEG_PosY,self.EEG_PosZ,self.brainDMask)
-            self.eegHeatmapVmax = self.eegHeatmap.max()
-            self.eegHeatmapVmin = self.eegHeatmap.min()
-           
-            self.ecogHeatmap = convolutionHeatmap(self.mapSpace,self.ECOG_Score,self.ECOG_PosX,self.ECOG_PosY,self.ECOG_PosZ,self.brainDMask)
-            self.ecogHeatmapVmax = self.ecogHeatmap.max()
-            self.ecogHeatmapVmin = self.ecogHeatmap.min()
+            self.Heatmap = convolutionHeatmap(self.timestep, self.row, self.col, self.depth, self.Score, self.PosX, self.PosY, self.PosZ, self.brainDMask)
+            self.HeatmapVmax = self.Heatmap.max()
+            self.HeatmapVmin = self.Heatmap.min()
 
             self.HEATMAP_FLAG = YES_READIN
 
@@ -153,48 +130,24 @@ class HeatMap:
         if not self.HEATMAP_FLAG:
             print('Requiring heatmap not generated')
         else:
-            my_model = MyModel(self.brainMask,self.eegHeatmap,self.ecogHeatmap,self.EEG_PosX,self.EEG_PosY,self.EEG_PosZ,self.ECOG_PosX,self.ECOG_PosY,self.ECOG_PosZ,self.eegHeatmapVmin,self.eegHeatmapVmax,self.ecogHeatmapVmin,self.ecogHeatmapVmax)
+            my_model = MyModel(self.brainMask,self.Heatmap,self.PosX,self.PosY,self.PosZ,self.HeatmapVmin,self.HeatmapVmax,self.timestep)
             my_model.configure_traits()
         
 
 class MyModel(HasTraits):
-
-    def __init__(self,brain,eegheatmap,ecogheatmap,eeg_posx,eeg_posy,eeg_posz,ecog_posx,ecog_posy,ecog_posz,eegmin,eegmax,ecogmin,ecogmax):
-        HasTraits.__init__(self)
-        self.brain = brain
-        self.eegheatmap = eegheatmap
-        self.ecogheatmap = ecogheatmap
-
-        self.eeg_posx = eeg_posx
-        self.eeg_posy = eeg_posy
-        self.eeg_posz = eeg_posz
-        self.ecog_posx = ecog_posx
-        self.ecog_posy = ecog_posy
-        self.ecog_posz = ecog_posz
-
-        self.eegmin = eegmin
-        self.eegmax = eegmax
-        self.ecogmin = ecogmin
-        self.ecogmax = ecogmax
-
-        self.source = SOURCE_IS_EEG
-        self.electrode = ELECTRODE_OFF
-
-
-        
     scene = Instance(MlabSceneModel,())
     plot = Instance(PipelineBase)
     
-    signalType = Button('EEG/ECoG')
+    signalType = Button('No Use')
     electrodesType = Button('On/Off Electrodes')
-    time = Range(0,0,0)  # Because we have no time series currently, so I directly set the time as one time step
+    time = Range(0,100,0)  # Because we have no time series currently, so I directly set the time as one time step
 
     view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene),
                     height=700, width=800, show_label=False),
                 VGroup(
                         '_', Item(name='time'),'_', 
                         HGroup(
-                            Item(name = 'signalType', label= 'Choose data sourse',show_label=True),
+                            Item(name = 'signalType', label= 'No Use',show_label=True),
                             Item(name = 'electrodesType', label = 'Electrodes',show_label=True)
                             ),
                         show_border=True
@@ -203,23 +156,39 @@ class MyModel(HasTraits):
                 resizable=True
                 )
 
+
+
+    def __init__(self,brainMask,heatmap,posx,posy,posz,vmin,vmax,timeBoundary):
+        HasTraits.__init__(self)
+        self.brain = brainMask
+        self.Heatmap = heatmap
+
+        self.posx = posx
+        self.posy = posy
+        self.posz = posz
+
+        self.vmin = vmin
+        self.vmax = vmax
+
+        self.timeBoundary = timeBoundary
+
+        self.electrode = ELECTRODE_OFF
+
+
+
     @on_trait_change('time,scene.activated')
     def update_time(self):
-        if self.source == SOURCE_IS_EEG:
-            heatmap = self.eegheatmap
-            elecVmin = self.eegmin
-            elecVmax = self.eegmax
-        if self.source == SOURCE_IS_ECOG:
-            heatmap = self.ecogheatmap
-            elecVmin = self.ecogmin
-            elecVmax = self.ecogmax
+        if self.time < self.timeBoundary:
+            heatmap = self.Heatmap[self.time]
+        else:
+            heatmap = self.Heatmap[self.timeBoundary-1]
 
         if self.plot is None:
             background = self.scene.mlab.pipeline.scalar_field(self.brain)
             self.scene.mlab.pipeline.iso_surface(background,vmin=0,vmax=1,opacity=1.0,colormap='gray',figure=self.scene.mayavi_scene)
 
             heat = self.scene.mlab.pipeline.scalar_field(heatmap)
-            self.plot = self.scene.mlab.pipeline.volume(heat,vmax=elecVmin + .8*(elecVmax-elecVmin), vmin=elecVmin,figure=self.scene.mayavi_scene)
+            self.plot = self.scene.mlab.pipeline.volume(heat,vmax=self.vmin + .8*(self.vmax-self.vmin), vmin=self.vmin,figure=self.scene.mayavi_scene)
                 
             self.scene.mlab.view(azimuth=180,elevation=80,distance=350)
         else:
@@ -227,15 +196,10 @@ class MyModel(HasTraits):
 
     @on_trait_change('electrodesType') # switch electrodes visuliaztion on and off.
     def update_points(self):
-        if self.source == SOURCE_IS_EEG:
-            electronX = self.eeg_posx
-            electronY = self.eeg_posy
-            electronZ = self.eeg_posz
-        if self.source == SOURCE_IS_ECOG:
-            electronX = self.ecog_posx
-            electronY = self.ecog_posy
-            electronZ = self.ecog_posz                  
-
+        electronX = self.posx
+        electronY = self.posy
+        electronZ = self.posz
+               
         if self.electrode == ELECTRODE_OFF:
             self.electrode = ELECTRODE_ON
 
@@ -249,16 +213,9 @@ class MyModel(HasTraits):
             self.elec.stop()           
 
 
-    @on_trait_change('signalType') # change heatmap between EEG and ECoG
+    @on_trait_change('signalType') # change heatmap between EEG and ECoG... not anymore
     def update_source(self):
-        if self.source == SOURCE_IS_EEG:
-            self.source = SOURCE_IS_ECOG
-            heatmap = self.ecogheatmap
-            self.plot.mlab_source.scalars = heatmap
-        else:
-            self.source = SOURCE_IS_EEG
-            heatmap = self.eegheatmap
-            self.plot.mlab_source.scalars = heatmap
+        pass
 
 
 
@@ -267,15 +224,11 @@ class MyModel(HasTraits):
 
     
 if __name__ == '__main__':
+    
     brain = './standard.cerebrum.mask.nii.gz'
-    Test = HeatMap(brain)
-
-    Test.readEEG_Pos('eegcorr.csv')
-    Test.readEEG_Score('./Power_Jake/EEG1.csv')
-
-    Test.readECOG_Pos('eegcorr.csv')  #no ECoG coordinates currently, use EEG's for subsitute temporarily
-    Test.readECOG_Score('./Power_Jake/ECOG001.csv')
-
+    posTable = ''
+    scoreTable = ''
+    Test = HeatMap(brain,posTable,scoreTable)
     Test.generateHeatmap()
-
     Test.show()
+    
