bbf7d313a03e12216c0cd1befa665531387ae8ce
aogarlid <aogarlid@gmail.com>
2018-12-11 16:29:42 -0800

    format Jake.py
    Signed-off-by: aogarlid <aogarlid@gmail.com>

--- anders/mod/Jake.py
+++ anders/mod/Jake.py
@@ -5,7 +5,8 @@ import re
 import csv
 
 def run(cursor, filename, startTime, stopTime, interval, startFrequency, stopFrequency):
-    os.system("..\\Jake\\Power_Analysis_2.exe" + " " + filename + " " + startTime + " " + stopTime + " " + interval + " " + startFrequency + " " + stopFrequency)
+    os.system("..\\Jake\\Power_Analysis_2.exe" + " " + filename + " " + startTime + " " + stopTime + " " + interval +
+              " " + startFrequency + " " + stopFrequency)
     #new_file = re.sub(r"(.*).([^.]*)$", r"\1.csv", (filename))
     #with open(new_file, "r") as f:
     #    csvreader = csv.reader(f)
