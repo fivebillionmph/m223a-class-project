23ec513372ced299a746a0b413d8033efcea7a48
aogarlid <aogarlid@gmail.com>
2018-12-14 15:25:44 -0800

    delete old folder, README
    Signed-off-by: aogarlid <aogarlid@gmail.com>

--- anders/mod/config.py
+++
@@ -1,3 +0,0 @@
-brainsuite_cortical_extraction_script = "/Applications/BrainSuite18a/bin/cortical_extraction.sh"
-
-is_windows = False
--- anders/mod/util.py
+++
@@ -1,8 +0,0 @@
-import os.path
-
-def inputFilepath(text):
-    while True:
-        path = input(text)
-        if os.path.isfile(path):
-            return path
-        print("Invalid file path")
