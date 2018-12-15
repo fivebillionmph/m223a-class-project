75726cb93290ca4ce3f54086e117b89c36f595e0
aogarlid <aogarlid@gmail.com>
2018-12-15 00:04:39 -0800

    update pac.py
    Signed-off-by: aogarlid <aogarlid@gmail.com>

--- mod/mohammad/pac.py
+++ mod/mohammad/pac.py
@@ -1,7 +1,9 @@
 import matlab.engine
-def run():
-    eng = matlab.engine.start_matlab()
-    pac = eng.pac(r'file', matlab.double([2, 14]), matlab.double([40, 150]), float(5), float(2), float(4), float(2),
-                   float(1), float(0.5), nargout=1)
-    print(pac)
 
+def run(eeg_file, band_lo, band_hi, ch_count,
+                             ch_first, ch_last, sigtime_total, sigtime_window, sigtime_step):
+    eng = matlab.engine.start_matlab()
+    print("Checkpoint 3")
+    pac = eng.pac(eeg_file, matlab.double(band_lo), matlab.double(band_hi), float(ch_count), float(ch_first),
+                   float(ch_last), float(sigtime_total), float(sigtime_window), float(sigtime_step), nargout=2)
+    print("Checkpoint 4")
