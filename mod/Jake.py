ab0d525ebcf82e4a091cbfa74200ceb0d559d4cf
aogarlid <aogarlid@gmail.com>
2018-12-14 23:58:44 -0800

    update signal analysis
    Signed-off-by: aogarlid <aogarlid@gmail.com>

--- mod/Jake.py
+++ mod/Jake.py
@@ -5,8 +5,8 @@ SIGNAL ANALYSIS: Power Analysis (Jake "method 3")
 import os
 
 def run(cursor, filename, startTime, stopTime, interval, startFrequency, stopFrequency):
-    os.system("..\\power_analysis\\Power_Analysis_2.exe" + " " + filename + " " + startTime + " " + stopTime + " " + interval +
-              " " + startFrequency + " " + stopFrequency)
+    os.system("..\\power_analysis\\Power_Analysis_2.exe" + " " + filename + " " + startTime + " " + stopTime + " " +
+              interval + " " + startFrequency + " " + stopFrequency)
     #new_file = re.sub(r"(.*).([^.]*)$", r"\1.csv", (filename))
     #with open(new_file, "r") as f:
     #    csvreader = csv.reader(f)
